6f9ba355e0f2cea65216dac3d5e14d38876c520d
Arman-Kabir <armankabir8@gmail.com>
2024-03-13 15:30:40 +0600

    clock-app initialized

--- App.js
+++ App.js
@@ -1,49 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, Button, SafeAreaView } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
-  return (
-
-    <SafeAreaView style={styles.container}>
-      {/* <Text style={{ fontWeight: "bold", color: "white", fontSize: 28, backgroundColor: "blue", padding: 20 }}>Hello World</Text> */}
-  
-
-      <View style={[styles.box, styles.box1]} />
-      <View style={[styles.box, styles.box2]} />
-      <View style={[styles.box, styles.box3]} />
-
-
-    </SafeAreaView>
-
-  )
-};
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    flexDirection:'row',
-    // justifyContent:'space-between'
-    alignItems:'center'
-  },
-  box: {
-    height:100,
-    width:100,
-    // backgroundColor: 'red',
-  },
-  box1: {
-
-    backgroundColor: "yellow"
-  },
-  box2: {
-
-    backgroundColor: "green"
-  },
-  box3: {
-
-    backgroundColor: "orange",
-  }
-});
+    return (
+        <View></View>
+    )
+}
